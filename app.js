7f2c4c20e912126f1a74466543634d531a6d61cc
AlexGhosTTT <Alexe_97@inbox.ru>
2017-04-13 12:55:22 +0300

    online-game-helicopters-attack/feature: add files

---
+++ app.js
@@ -0,0 +1,19 @@
+﻿window.addEventListener('load', () => {
+
+    let LinkButnGame = document.getElementById('LinkButnGame');
+
+    LinkButnGame.addEventListener('click', () => {
+
+        location.href = "https://whispering-brook-21659.herokuapp.com/";
+
+    });
+
+    let LinkButnPresentation = document.getElementById('LinkButnPresentation');
+
+    LinkButnPresentation.addEventListener('click', () => {
+
+
+
+    });
+
+});
